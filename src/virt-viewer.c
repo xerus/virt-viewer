xerus/virt-viewer

commit a62827d28c6b69e90102e4c1c8043cbddad8929a

virt-viewer: ensure we close when seeing domain stop event
Normally virt-viewer relies on the VNC/SPICE widget seeing
an EOF on its underlying connection to detect when the
session is closed.

When tunnelling to a remote guest over SSH though, this
EOF can be delayed for a very long time, leaving a dead
session open.

This can be seen with

   virt-viewer -c qemu+ssh://remotehost/system guestname

when on the remote shell run

   virsh destroy guestname

and notice that virt-viewer does not see the shutdown
immediately.

When we get a domain stopped event we know the session
should be dead, so forceably close it, if not already
closed.

Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

src/virt-viewer.c
@@ -664,6 +664,7 @@ virt_viewer_domain_event(virConnectPtr conn G_GNUC_UNUSED,
 {
     VirtViewer *self = opaque;
     VirtViewerApp *app = VIRT_VIEWER_APP(self);
+    VirtViewerSession *session;
     GError *error = NULL;
 
     g_debug("Got domain event %d %d", event, detail);
@@ -673,7 +674,9 @@ virt_viewer_domain_event(virConnectPtr conn G_GNUC_UNUSED,
 
     switch (event) {
     case VIR_DOMAIN_EVENT_STOPPED:
-        //virt_viewer_deactivate(self);
+        session = virt_viewer_app_get_session(app);
+        if (session != NULL)
+            virt_viewer_session_close(session);
         break;
 
     case VIR_DOMAIN_EVENT_STARTED:

commit cd27808c72dfe86b5218fce90c157ca1ac721f6e

virt-viewer: Ensure to not close during migration
Take a look at the shutdown event detail before killing
the connection.

Regression since commit a62827d28c6b69e90102e4c1c8043cbddad8929a

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1442929
Signed-off-by: Pavel Grunt <pgrunt@redhat.com>

src/virt-viewer.c
@@ -52,6 +52,10 @@
 #include "virt-viewer-auth.h"
 #include "virt-viewer-util.h"
 
+#ifdef HAVE_SPICE_GTK
+#include "virt-viewer-session-spice.h"
+#endif
+
 struct _VirtViewerPrivate {
     char *uri;
     virConnectPtr conn;
@@ -737,6 +741,13 @@ virt_viewer_domain_event(virConnectPtr conn G_GNUC_UNUSED,
     switch (event) {
     case VIR_DOMAIN_EVENT_STOPPED:
         session = virt_viewer_app_get_session(app);
+#ifdef HAVE_SPICE_GTK
+        /* do not disconnect due to migration */
+        if (detail == VIR_DOMAIN_EVENT_STOPPED_MIGRATED &&
+            VIRT_VIEWER_IS_SESSION_SPICE(session)) {
+            break;
+        }
+#endif
         if (session != NULL)
             virt_viewer_session_close(session);
         break;